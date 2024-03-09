a19a1f60acd3a9b57f20ac0d927870bcb3a0846a
Clement Gangloff <cgangloff@bird.com>
2024-03-09 22:55:26 +0100

    feat: add french and english translation

--- Service/AIRequestProcessor.php
+++ Service/AIRequestProcessor.php
@@ -2,10 +2,13 @@
 
 namespace Opengento\AdminAssistant\Service;
 
+use JsonException;
+use Magento\Framework\Exception\LocalizedException;
 use Opengento\OpenAIConnector\Api\GPTCompletionsInterface;
 use Opengento\AdminAssistant\Api\Service\AIRequestProcessorInterface;
 use Magento\Framework\App\ResourceConnection;
 use Opengento\OpenAIConnector\Model\Exception\OpenAICompletionException;
+use const JSON_THROW_ON_ERROR;
 
 class AIRequestProcessor implements AIRequestProcessorInterface
 {
@@ -17,31 +20,30 @@ class AIRequestProcessor implements AIRequestProcessorInterface
     ) {
     }
 
+    /**
+     * @throws OpenAICompletionException
+     * @throws JsonException
+     * @throws LocalizedException
+     */
     public function execute(string $userQuestion): string
     {
-        try {
-            $request = $this->generateInitialRequest($userQuestion);
-        } catch (OpenAICompletionException $e) {
-            return 'Error' . $e->getMessage();
-        }
+        $sqlRequest = $this->generateInitialRequest($userQuestion);
 
-        $requestResponse = null;
-        $try             = 0;
+        $sqlResponse = null;
+        $try         = 0;
         while ($try < static::MAX_CALL) {
             try {
-                $requestResponse = $this->resourceConnection->getConnection()->fetchAll($request);
+                $sqlResponse = $this->resourceConnection->getConnection()->fetchAll($sqlRequest);
                 break;
             } catch (\Exception $e) {
-                $request = $this->fixRequest($userQuestion, $e->getMessage());
+                $sqlRequest = $this->fixRequest($userQuestion, $e->getMessage());
                 $try++;
             }
         }
 
-        try {
-            return $this->interpretDataDependingOnUserPrompt($userQuestion, $requestResponse);
-        } catch (OpenAICompletionException $e) {
-            return 'Error' . $e->getMessage();
-        }
+        return $sqlResponse
+            ? $this->interpretDataDependingOnUserPrompt($userQuestion, $sqlResponse)
+            : throw new LocalizedException(__('ChatGPT is unable to handle the question'));
     }
 
     /**
@@ -50,10 +52,10 @@ class AIRequestProcessor implements AIRequestProcessorInterface
     protected function generateInitialRequest(string $userQuestion): string
     {
         return $this->GPTCompletions->getGPTCompletions(
-            prompt     : "
-Je me pose cette question : $userQuestion.
-Ecrit moi uniquement la requete SQL, n'ajoute pas d'explications !
-",
+            prompt     : __(
+                             "Je me pose cette question : %1. Ecrit moi uniquement la requete SQL, n'ajoute pas d'explications !",
+                             $userQuestion
+                         )->render(),
             temperature: 0
         );
     }
@@ -64,26 +66,27 @@ Ecrit moi uniquement la requete SQL, n'ajoute pas d'explications !
     protected function fixRequest(string $userQuestion, string $errorMessage): string
     {
         return $this->GPTCompletions->getGPTCompletions(
-            prompt     : "
-Je me pose cette question : $userQuestion.
-Base toi sur cette erreur : $errorMessage.
-Ecrit moi uniquement la requete SQL, n'ajoute pas d'explications !
-",
+            prompt     : __(
+                             "Je me pose cette question : %1. Base toi sur cette erreur : %2. Ecrit moi uniquement la requete SQL, n'ajoute pas d'explications !",
+                             $userQuestion,
+                             $errorMessage
+                         )->render(),
             temperature: 0
         );
     }
 
     /**
      * @throws OpenAICompletionException
+     * @throws JsonException
      */
-    protected function interpretDataDependingOnUserPrompt(string $userQuestion, array $requestData): string
+    protected function interpretDataDependingOnUserPrompt(string $userQuestion, array $sqlResponse): string
     {
         return $this->GPTCompletions->getGPTCompletions(
-            prompt     : "
-Je me pose cette question : $userQuestion.
-J'ai cette donnée comme résultat :" . json_encode($requestData) . ".
-Réponds à la question de manière concis en intérpretant les données. Oublie le coté code, tu t'adresse à un commercial, un responsable de marketing ou à un responsable de communication.
-",
+            prompt     : __(
+                             "Je me pose cette question : %1. J'ai cette donnée comme résultat : %2. Réponds à la question de manière concis en intérpretant les données. Oublie le coté code, tu t'adresse à un commercial, un responsable de marketing ou à un responsable de communication.",
+                             $userQuestion,
+                             json_encode($sqlResponse, JSON_THROW_ON_ERROR)
+                         )->render(),
             temperature: 0
         );
     }
