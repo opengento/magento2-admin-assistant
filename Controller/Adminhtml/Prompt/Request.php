287aa22d3a0c1beee7beb87b9f3f3ef0bfe0b497
Clement Gangloff <cgangloff@bird.com>
2024-03-10 02:39:49 +0100

    feat: add chat in BO

--- Controller/Adminhtml/Prompt/Request.php
+++ Controller/Adminhtml/Prompt/Request.php
@@ -7,7 +7,7 @@ namespace Opengento\AdminAssistant\Controller\Adminhtml\Prompt;
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
 use Magento\Backend\App\Response\Http\FileFactory;
-use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Controller\Result\JsonFactory;
 use Opengento\AdminAssistant\Api\Service\CsvFileNameGeneratorInterface;
 use Opengento\AdminAssistant\Api\Service\PromptProcessorInterface;
 use Opengento\AdminAssistant\Service\CsvFormatter;
@@ -19,7 +19,8 @@ class Request extends Action
         protected PromptProcessorInterface $promptProcessor,
         protected FileFactory $fileFactory,
         protected CsvFileNameGeneratorInterface $csvFileNameGenerator,
-        protected CsvFormatter $csvFormatter
+        protected CsvFormatter $csvFormatter,
+        protected JsonFactory $jsonFactory
     ) {
         parent::__construct($context);
     }
@@ -28,20 +29,27 @@ class Request extends Action
     {
         $prompt = $this->getRequest()->getParam('prompt');
         try  {
+            $resultJson = $this->jsonFactory->create();
             $sqlData = $this->promptProcessor->getSqlDataFromPrompt($prompt);
 
-            if (\count($sqlData) > 10) {
-                return $this->fileFactory->create(
-                    $this->csvFileNameGenerator->generateFileName(),
-                    $this->csvFormatter->formatSqlDataToCsv($sqlData),
-                    DirectoryList::VAR_DIR,
-                    'text/csv'
-                );
+            if (\count($sqlData) > 2) {
+                $response =  [
+                    'fileName' => $this->csvFileNameGenerator->generateFileName(),
+                    'fileContent' => $this->csvFormatter->formatSqlDataToCsv($sqlData),
+                ];
+
+                return $resultJson->setData($response);
             }
 
-            return $this->promptProcessor->interpretDataDependingOnUserPrompt($prompt, $sqlData);
-        } catch (\Exception) {
-            // TODO: Afficher message erreur
+            $response = [
+                'answer' => $this->promptProcessor->interpretDataDependingOnUserPrompt($prompt, $sqlData)
+            ];
+            return $resultJson->setData($response);
+        } catch (\Exception $e) {
+            $response = [
+                'error' => $e->getMessage()
+            ];
+            return $resultJson->setData($response);
         }
     }
 }
